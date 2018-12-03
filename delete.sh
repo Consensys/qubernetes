201bba817df5755b044d4ea465aa3cbd6ac09118
Libby Kent <viskovitzzz@gmail.com>
2018-12-03 11:30:08 -0500

    Add raft and istanbul 7nodes examples k8s resources.
    * Add support for generating k8s resources yaml for the 7 nodes examples for both raft
      and istanbul https://github.com/jpmorganchase/quorum-examples/tree/master/examples/7nodes.
    * Generate the k8s resource yaml for raft `7nodes/raft-7nodes/k8s-yaml` and istanbul
      `7nodes/istanbul-7nodes/k8s-yaml`.
    * By default `qubernetes.yaml` --> `config/qubernets.yaml`

--- delete.sh
+++ delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-qctl="kubectl --namespace=quorum-test  --kubeconfig=/home/libby/.go/src/github.com/ethereum/k8-quorum/k8_config --insecure-skip-tls-verify "
+qctl="kubectl --namespace=quorum-test  --kubeconfig={PATH/TO/YOUR}/k8_config --insecure-skip-tls-verify "
 
 $qctl delete -f out/quorum-shared-config.yaml
 $qctl delete -f out/quorum-services.yaml
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-qctl="kubectl --namespace=quorum-test  --kubeconfig=/home/libby/.go/src/github.com/ethereum/k8-quorum/k8_config --insecure-skip-tls-verify "
-
+qctl="kubectl --namespace=quorum-test  --kubeconfig={PATH/TO/YOUR}/k8_config --insecure-skip-tls-verify "
 $qctl create -f out/quorum-shared-config.yaml
 $qctl create -f out/quorum-services.yaml
 $qctl create -f out/quorum-keyconfigs.yaml

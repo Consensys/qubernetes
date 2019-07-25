dfeca9df9e548aa32c686a9a79d9d51f91afe003
Libby Kent <viskovitzzz@gmail.com>
2019-07-25 11:36:18 -0400

    Helper scripts to create and take down tunnels.
    Set up tunnels for a minikube quorum deployment, so that applications, such as cakeshop,
    can connected to the transaction manager without mapping proxied IP addresses,
    e.g. minikube sets up proxy IPs, but these will be different from the IPs used by the internal
    cluster, mainly in curl http://$IP:9001/partyinfo.
    
    Does not prompt to verify the authenticity of the host and adds the host to known_hosts on the first connection.

---
+++ helpers/set_up_tunnels.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# set up tunnels for a minikube quorum deployment, so
+# that applications, such as cakeshop, can connected
+# to the transaction manager without mapping proxied
+# IP addresses, e.g. minikube sets up proxy IPs, but
+# these will be different from the IPs used by the internal
+# cluster, mainly in curl http://$IP:9001/partyinfo.
+IPS=$(kubectl get svc | grep "quorum" | awk '{ print $3}')
+MINI_IP=$(minikube ip)
+for IP in $IPS
+do
+ echo $IP
+ sudo ifconfig lo0 alias $IP
+ echo "setting up port forwarding mini $MINI_IP"
+ # don't prompt for 'The authenticity of host' / known_host on first connection.
+ ssh -N  -o "StrictHostKeyChecking no" -L $IP:9001:$IP:9001 -i ~/.minikube/machines/minikube/id_rsa docker@$MINI_IP  &
+done
+
+for IP in $IPS
+do
+ echo "curl http://$IP:9001/partyinfo"
+done
---
+++ helpers/take_down_tunnels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# helper script to take down tunnels that have been
+# setup by `set_up_tunnel.sh
+IPS=$(kubectl get svc | grep "quorum" | awk '{ print $3}')
+for IP in $IPS
+do
+ sudo ifconfig lo0 -alias $IP
+done
+
+PIDS=$(ps -ef | grep "ssh -N -L" | awk '{print $2}')
+for PID in $PIDS
+do
+ echo $PID
+ kill $PID
+done

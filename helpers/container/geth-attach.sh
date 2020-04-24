4e36cf214d4a1f22a0687f0814c0986e7f01a24a
Libby Kent <viskovitzzz@gmail.com>
2020-04-24 12:24:58 -0400

    Add support for k8s securityContext on deployments.
    * if the k8s securityContext is set in the config add it to the deployments.
    see an example: examples/config/qubernetes-security-context.yaml
    ks:
      securityContext:
        runAsUser: 430
    ...
    note: not supporting the additions of capabilities at this time.
    
    * add --datadir to all geth attach commands, as if the securitycontext
    is set --datadir is required or else the following error will be seen:
    Fatal: Failed to start the JavaScript console: mkdir /.ethereum: permission denied

--- helpers/container/geth-attach.sh
+++ helpers/container/geth-attach.sh
@@ -4,4 +4,4 @@
 # outside the container
 # kubectl exec -it $POD -c quorum -- /geth-helpers/geth-attach.sh
 echo "connecting to geth $QHOME"
-geth attach $QHOME/dd/geth.ipc
+geth attach --datadir $QUORUM_DATA_DIR $QUORUM_DATA_DIR/geth.ipc
